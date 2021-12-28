57c3981adbcddf57b5a2bb78a556728513c335cb
DanielPhilosoph <danielph9@gmail.com>
2021-12-28 21:58:45 +0200

    done

--- users-service/repository/repository.js
+++ users-service/repository/repository.js
@@ -2,9 +2,9 @@
 //
 //  Exposes a single function - 'connect', which returns
 //  a connected repository. Call 'disconnect' on this object when you're done.
-'use strict';
+"use strict";
 
-var mysql = require('mysql');
+var mysql = require("mysql2");
 
 //  Class which holds an open connection to a repository
 //  and exposes some simple functions for accessing data.
@@ -15,45 +15,51 @@ class Repository {
 
   getUsers() {
     return new Promise((resolve, reject) => {
+      this.connection.query(
+        "SELECT email, phone_number FROM directory",
+        (err, results) => {
+          if (err) {
+            return reject(
+              new Error("An error occured getting the users: " + err)
+            );
+          }
 
-      this.connection.query('SELECT email, phone_number FROM directory', (err, results) => {
-        if(err) {
-          return reject(new Error('An error occured getting the users: ' + err));
+          resolve(
+            (results || []).map((user) => {
+              return {
+                email: user.email,
+                phone_number: user.phone_number,
+              };
+            })
+          );
         }
-
-        resolve((results || []).map((user) => {
-          return {
-            email: user.email,
-            phone_number: user.phone_number
-          };
-        }));
-      });
-
+      );
     });
   }
 
   getUserByEmail(email) {
-
     return new Promise((resolve, reject) => {
-
       //  Fetch the customer.
-      this.connection.query('SELECT email, phone_number FROM directory WHERE email = ?', [email], (err, results) => {
+      this.connection.query(
+        "SELECT email, phone_number FROM directory WHERE email = ?",
+        [email],
+        (err, results) => {
+          if (err) {
+            return reject(
+              new Error("An error occured getting the user: " + err)
+            );
+          }
 
-        if(err) {
-          return reject(new Error('An error occured getting the user: ' + err));
+          if (results.length === 0) {
+            resolve(undefined);
+          } else {
+            resolve({
+              email: results[0].email,
+              phone_number: results[0].phone_number,
+            });
+          }
         }
-
-        if(results.length === 0) {
-          resolve(undefined);
-        } else {
-          resolve({
-            email: results[0].email,
-            phone_number: results[0].phone_number
-          });
-        }
-
-      });
-
+      );
     });
   }
 
@@ -65,10 +71,11 @@ class Repository {
 //  One and only exported function, returns a connected repo.
 module.exports.connect = (connectionSettings) => {
   return new Promise((resolve, reject) => {
-    if(!connectionSettings.host) throw new Error("A host must be specified.");
-    if(!connectionSettings.user) throw new Error("A user must be specified.");
-    if(!connectionSettings.password) throw new Error("A password must be specified.");
-    if(!connectionSettings.port) throw new Error("A port must be specified.");
+    if (!connectionSettings.host) throw new Error("A host must be specified.");
+    if (!connectionSettings.user) throw new Error("A user must be specified.");
+    if (!connectionSettings.password)
+      throw new Error("A password must be specified.");
+    if (!connectionSettings.port) throw new Error("A port must be specified.");
 
     resolve(new Repository(mysql.createConnection(connectionSettings)));
   });
